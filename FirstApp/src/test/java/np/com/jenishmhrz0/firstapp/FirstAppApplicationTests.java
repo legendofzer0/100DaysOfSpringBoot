0d4af146a6e4f01dcede28020f881ac86cf84400
Jenish Maharjan <aayur50@gmail.com>
2025-08-28 07:33:31 +0545

    first commit

---
+++ FirstApp/src/test/java/np/com/jenishmhrz0/firstapp/FirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.jenishmhrz0.firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
